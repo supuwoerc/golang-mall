33622d6e95df254492ecab216e32244a1149f2ac
zhangqm <qimeng_zhang@intsig.net>
2023-03-13 22:46:01 +0800

    配置文件抽离

--- server/main.go
+++ server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"server/component/config"
 	_ "server/component/mysql" //初始化数据库
 )
 
 func main() {
 	g := gin.New()
 	RouterRegister(g) //路由
-	_ = g.Run(":8080")
+	_ = g.Run(config.Config.GetString("server.host"))
 }
